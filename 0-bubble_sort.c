f6ba8cd08daa92ac6ace183cde2631a0266b3cfc
Nasser Jamal <nasserjamal30@gmail.com>
2022-10-09 10:58:50 +0300

    bubble sort improved

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,9 +11,11 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j = 0;
 	int temp = 0;
+	int isSwap = 0;
 
 	for (i = 0; i < size; i++)
 	{
+		isSwap = 0;
 		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -21,10 +23,14 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j + 1];
 				array[j + 1] = array[j];
 				array[j] = temp;
+				isSwap = 1;
 				print_array(array, size);
 			}
 		}
-
+		if (!isSwap)
+		{
+			return;
+		}
 	}
 
 }
