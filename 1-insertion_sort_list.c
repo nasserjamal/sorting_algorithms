bf90ebed44f0897d4d0eb8dd021f85edb0005fd9
Nasser Jamal <nasserjamal30@gmail.com>
2022-10-09 17:32:26 +0300

    Insertion sort list

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+
+/**
+ * insertion_sort_list - Sorts list linked listusing
+ * insertion sort
+ *
+ * @list: The list to be sorted
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *node = (*list);
+	listint_t *nodeCurrent = NULL;
+	listint_t *nodePrevious = NULL;
+	listint_t *tempB;
+	listint_t *tempA;
+
+
+	while (node != NULL)
+	{
+		nodeCurrent = node;
+		while (nodeCurrent->prev != NULL)
+		{
+			nodePrevious = nodeCurrent->prev;
+			if (nodeCurrent->n < nodePrevious->n)
+			{
+				/*Swap*/
+				tempA = nodeCurrent->next;
+				tempB = nodePrevious->prev;
+				nodeCurrent->next = nodePrevious;
+				nodeCurrent->prev = tempB;
+				nodePrevious->prev = nodeCurrent;
+				nodePrevious->next = tempA;
+				if (tempA != NULL)
+					tempA->prev = nodePrevious;
+				if (tempB != NULL)
+					tempB->next = nodeCurrent;
+				else
+					*list = nodeCurrent;
+				print_list(*list);
+			} else
+				break;
+		}
+		node = node->next;
+	}
+
+}
