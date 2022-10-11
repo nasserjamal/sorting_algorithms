3a48e19c5603022196c2499663d3db42bedf6ff8
Nasser Jamal <nasserjamal30@gmail.com>
2022-10-11 15:45:20 +0300

    shell sort fixed

--- sort.h
+++ sort.h
@@ -28,5 +28,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void sort_arr(int *array, size_t start, size_t end, size_t size);
+void swap_ints(int *a, int *b);
+void shell_sort(int *array, size_t size);
 
 #endif
